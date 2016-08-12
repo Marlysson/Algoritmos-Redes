31f63b0d2a08be8acbeb55d1306c6b8aa5e99c8e
Marlysson Silva <marlysson5@gmail.com>
2016-08-12 14:44:49 -0300

    Adding tests of some methods.

--- Hamming/teste.py
+++ Hamming/teste.py
@@ -20,7 +20,7 @@ class TestFrame(unittest.TestCase):
 class TestHamming(unittest.TestCase):
 
 	def setUp(self):
-		self.hamming = Hamming()
+		self.hamming = Hamming("par")
 
 	def test_power_of_two_valids(self):
 		valids = [1,2,4,8,16,32,64]
@@ -37,25 +37,40 @@ class TestHamming(unittest.TestCase):
 		for value in invalids:
 			self.assertFalse(self.hamming.is_power(value,2))
 
-	@unittest.skip("Test of a dependence ( private method )")
-	def test_create_list_with_spaces_to_power_of_two(self):
-		frame1 = Frame("011")
-		frame2 = Frame("10101101011100110")
-
-		return_frame_1 = ["","",0,"",1,1]
-		return_frame_2 = ["","",1,"",0,1,0,"",1,1,0,1,0,1,1,"",1,0,0,1,1,0]
-
-		self.assertEqual(return_frame_1,self.hamming.encode(frame1))
-		self.assertEqual(return_frame_2,self.hamming.encode(frame2))
-
-
-	@unittest.skip("skipped")
 	def test_frame_encoded_should_be_correct(self):
-		frame = Frame("0111")
+		frame1 = Frame("011")
+		frame2 = Frame("11100101010110")
+		frame3 = Frame("0100")
 
-		encoded = self.hamming.encode(frame)
+		frame1_encoded = self.hamming.encode(frame1)
+		frame2_encoded = self.hamming.encode(frame2)
+		frame3_encoded = self.hamming.encode(frame3)
 
-		self.assertEqual(Frame("0001111"),encoded)
+		self.assertEqual(Frame("110011"),frame1_encoded)
+		self.assertEqual(Frame("1110110101010100110"),frame2_encoded)
+		self.assertNotEqual(Frame("1100110"),frame3_encoded)
+
+	def test_calculate_parity_of_dataset_bits(self):
+		#Global parity : par
+
+		dataset = [0,1,1,0]
+
+		parity = self.hamming.calculate_parity(dataset)
+
+		self.assertEqual(parity,0)
+
+	def test_calculate_parity_of_dataset_changing_parity_of_hamming(self):
+
+		dataset = [0,1,1,1]
+
+		parity_par = self.hamming.calculate_parity(dataset)
+
+		self.hamming.parity = "impar"
+
+		parity_impar = self.hamming.calculate_parity(dataset)
+
+		self.assertEqual(1,parity_par)
+		self.assertEqual(0,parity_impar)
 
 	@unittest.skip("skipped")
 	def test_frame_check_should_be_correct(self):
